bb48c40a6d04eb8ddddd6ec7987ad2c6c7485e2e
Andperman <andperman87649@gmail.com>
2024-09-26 20:18:51 +0200

    JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarMensaje() {
+    // Mensaje de agradecimiento
+    alert("Gracias por contactar conmigo");
+
+    // Evitar que el formulario se envíe
+    return false; // Cambia esto a 'true' si deseas que el formulario se envíe realmente
+}
